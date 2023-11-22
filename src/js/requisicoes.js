230df7c8bc9f72239b612f153676a105d3c1848d
Daniel Lima <daniellimaa05@gmail.com>
2023-11-22 07:32:08 -0300

    Requisição http POST completa

--- src/js/requisicoes.js
+++ src/js/requisicoes.js
@@ -1,13 +1,24 @@
 "use strict";
 const tableElement = document.querySelector("#tabela");
 const tbodyElement = document.querySelector("tbody");
+const form = new FormData(document.querySelector("#form-solicitacoes"));
+const url = "https://localhost:3000/resolvasuavoz/solicitacoes ";
 // Metodo HTTP GET
 const getSolicitacoes = async () => {
   try {
-    const response = await fetch(
-      "http://localhost:3000/resolvasuavoz/solicitacoes"
-    );
+    const response = await fetch(`${url}`);
     return await response.json();
+  } catch (error) {
+    return [];
+  }
+};
+const postSolicitacoes = async (res) => {
+  try {
+    await fetch(`${url}`, {
+      method: "POST",
+      body: form,
+    });
+    console.log(res.data);
   } catch (error) {
     return [];
   }
